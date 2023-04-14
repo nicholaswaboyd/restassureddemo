775fe4b2cf58eb0e72ed3e06ba8a57f4d9cc52fa
Nicholas Boyd <nicholaswaboyd@gmail.com>
2023-04-14 01:00:27 +0100

    first commit

---
+++ src/test/java/com/nwab/GistTests.java
@@ -0,0 +1,38 @@
+package com.nwab;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class GistTests 
+{
+    @Test
+    public void createNewGist()
+    {
+        assertTrue(true);
+
+    }
+
+    @Test
+    public void listAllGistsForUser()
+    {
+        assertTrue( true );
+        
+    }
+
+    @Test
+    public void updateExistingGist()
+    {
+
+    }
+
+    @Test
+    public void deleteGist()
+    {
+
+        
+    }
+
+    
+}
