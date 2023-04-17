c28ca9c7365dd2ee40e8662f446555428fc59642
Nicholas Boyd <nicholaswaboyd@gmail.com>
2023-04-17 19:33:15 +0100

    Added third test and made some cleanup changes

--- src/test/java/com/nwab/GistTests.java
+++ src/test/java/com/nwab/GistTests.java
@@ -11,26 +11,28 @@ import io.restassured.response.Response;
 public class GistTests 
 {
 
-
-    private static final String token = "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";
+    //Final variables
+    private static final String TOKEN = "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";
     private static final String API_VERSION = "2022-11-28";
+    private static final boolean IS_PUBLIC = false;
+    private static final String BASE_URI = "https://api.github.com/gists";
+    //Gist variables
     private static String description;
-    private static final boolean isPublic = false;
-    private static String fileName;
-    private static String fileContent;
-    private static final String baseUri = "https://api.github.com/gists";
-    private String gistID;
+    private static String gistID;
+    //For file 1
+    private static String file1Name;
+    private static String file1Content;
 
     @After
     public void cleanUpGists()
     {
         //Delete test gist
         Response response = RestAssured.given()
-                .header("Authorization", "Bearer " + token)
+                .header("Authorization", "Bearer " + TOKEN)
                 .header("Accept", "application/vnd.github+json")
                 .header("X-GitHub-Api-Version", API_VERSION)
                 .contentType(ContentType.JSON)
-                .delete(baseUri + "/" + gistID);
+                .delete(BASE_URI + "/" + gistID);
 
         Assert.assertEquals(204, response.getStatusCode());
     }
@@ -40,18 +42,19 @@ public class GistTests
     {
         //Set variable strings
         description = "Testing Gist Creation API";
-        fileName = "myfile.txt";
-        fileContent = "The is a test Gist";
+        file1Name = "myfile.txt";
+        file1Content = "The is a test Gist";
 
         //Given I sent a request to create a Gist
         Response response = RestAssured.given()
-                .header("Authorization", "Bearer " + token)
+                .header("Authorization", "Bearer " + TOKEN)
                 .header("Accept", "application/vnd.github+json")
                 .header("X-GitHub-Api-Version", API_VERSION)
                 .contentType(ContentType.JSON)
-                .body("{\"description\":\"" + description + "\",\"public\":" + isPublic + ",\"files\":{\"" + fileName + "\":{\"content\":\"" + fileContent + "\"}}}")
-                .post(baseUri);
+                .body("{\"description\":\"" + description + "\",\"public\":" + IS_PUBLIC + ",\"files\":{\"" + file1Name + "\":{\"content\":\"" + file1Content + "\"}}}")
+                .post(BASE_URI);
 
+        //variable stored for cleanup after tests
         gistID = response.jsonPath().getString("id");
 
         //Then the API returns a 201 code and the Gist is created
@@ -65,27 +68,27 @@ public class GistTests
     {
         //Set variable strings
         description = "Testing Gist Creation API";
-        fileName = "myfile.txt";
-        fileContent = "The is a test Gist";
+        file1Name = "myfile.txt";
+        file1Content = "The is a test Gist";
 
         //Given I have created a Gist
-        Response creationResponse = RestAssured.given()
-                .header("Authorization", "Bearer " + token)
+        Response response = RestAssured.given()
+                .header("Authorization", "Bearer " + TOKEN)
                 .header("Accept", "application/vnd.github+json")
                 .header("X-GitHub-Api-Version", API_VERSION)
                 .contentType(ContentType.JSON)
-                .body("{\"description\":\"" + description + "\",\"public\":" + isPublic + ",\"files\":{\"" + fileName + "\":{\"content\":\"" + fileContent + "\"}}}")
-                .post(baseUri);
+                .body("{\"description\":\"" + description + "\",\"public\":" + IS_PUBLIC + ",\"files\":{\"" + file1Name + "\":{\"content\":\"" + file1Content + "\"}}}")
+                .post(BASE_URI);
 
-        gistID = creationResponse.jsonPath().getString("id");
+        gistID = response.jsonPath().getString("id");
 
         //When I query all Gists for a user
-        Response response = RestAssured.given()
-                .header("Authorization", "Bearer " + token)
+                 response = RestAssured.given()
+                .header("Authorization", "Bearer " + TOKEN)
                 .header("Accept", "application/vnd.github+json")
                 .header("X-GitHub-Api-Version", API_VERSION)
                 .contentType(ContentType.JSON)
-                .get(baseUri);
+                .get(BASE_URI);
         
         //Then the ID for the created Gist is present in the response
         Assert.assertTrue(response.getBody().asString().contains(gistID));
@@ -95,7 +98,38 @@ public class GistTests
     @Test
     public void updateExistingGist()
     {
+        //Set variable strings
+        description = "Testing Gist Creation API";
+        file1Name = "myFile1.txt";
+        file1Content = "This file will be updated";
+        String file1ContentUpdated = "Text succesfully updated.";
+        String file2Name = "myFile2.txt";
+        String file2Content = "This file will be deleted";
 
+        //Given I have created a Gist
+        Response response = RestAssured.given()
+                .header("Authorization", "Bearer " + TOKEN)
+                .header("Accept", "application/vnd.github+json")
+                .header("X-GitHub-Api-Version", API_VERSION)
+                .contentType(ContentType.JSON)
+                .body("{\"description\":\"" + description + "\",\"public\":" + IS_PUBLIC + ",\"files\":{\"" + file1Name + "\":{\"content\":\"" + file1Content + "\"},\"" + file2Name + "\":{\"content\":\"" + file2Content + "\"}}}")
+                .post(BASE_URI);
+        
+        gistID = response.jsonPath().getString("id");
+        Assert.assertEquals(201, response.getStatusCode());
+
+        //When I update file1 and delete file2
+                response = RestAssured.given()
+                .header("Authorization", "Bearer " + TOKEN)
+                .header("Accept", "application/vnd.github+json")
+                .header("X-GitHub-Api-Version", API_VERSION)
+                .contentType(ContentType.JSON)
+                .body("{\"files\":{\""+ file1Name +"\":{\"content\":\""+ file1ContentUpdated +"\"},\""+ file2Name +"\":null}}")
+                .patch(BASE_URI + "/" + gistID);
+        //Then these changes are successful
+        Assert.assertEquals(200, response.getStatusCode());
+        Assert.assertTrue(response.getBody().asString().contains(file1ContentUpdated));
+        Assert.assertFalse(response.getBody().asString().contains(file2Name));
     }
 
     @Test
