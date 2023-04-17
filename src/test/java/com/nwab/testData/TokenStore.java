fceea31b0f059633288efb82738dfb71dc001efb
Nicholas Boyd <nicholaswaboyd@gmail.com>
2023-04-17 21:02:03 +0100

    Added token store and generated new token

---
+++ src/test/java/com/nwab/testData/TokenStore.java
@@ -0,0 +1,8 @@
+package com.nwab.testData;
+
+public class TokenStore {
+    public static String getToken(){
+        String token = "INSERT TOKEN HERE";
+        return token;
+    }
+}
--- src/test/java/com/nwab/testScripts/GistTests.java
+++ src/test/java/com/nwab/testScripts/GistTests.java
@@ -1,9 +1,11 @@
-package com.nwab;
+package com.nwab.testScripts;
 
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.Ignore;
+
+import com.nwab.testData.TokenStore;
+
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -13,7 +15,7 @@ public class GistTests
 {
 
     //Final variables
-    private static final String TOKEN = "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";
+    private static final String TOKEN = TokenStore.getToken();
     private static final String API_VERSION = "2022-11-28";
     private static final boolean IS_PUBLIC = false;
     private static final String BASE_URI = "https://api.github.com/gists";
@@ -22,7 +24,7 @@ public class GistTests
     private static String gistID;
     //For file 1
     private static String file1Name = "myfile.txt";
-    private static String file1Content = "This file will be updated";
+    private static String file1Content = "The is a test gist";
     //For skipping cleanup when gists have already been deleted
     private boolean skipCleanup = false;
 
@@ -45,9 +47,6 @@ public class GistTests
     @Test
     public void testCreateNewGist()
     {
-        //Set variable strings
-        file1Content = "The is a test Gist";
-
         //Given I sent a request to create a Gist
         Response response = RestAssured.given()
                 .header("Authorization", "Bearer " + TOKEN)
@@ -69,9 +68,6 @@ public class GistTests
     @Test
     public void listAllGistsForUser()
     {
-        //Set variable strings
-        file1Content = "The is a test Gist";
-
         //Given I have created a Gist
         Response response = RestAssured.given()
                 .header("Authorization", "Bearer " + TOKEN)
@@ -100,6 +96,7 @@ public class GistTests
     public void updateExistingGist()
     {
         //Set variable strings
+        file1Content = "This file will be updated";
         String file1ContentUpdated = "Text succesfully updated.";
         String file2Name = "myFile2.txt";
         String file2Content = "This file will be deleted";
@@ -133,7 +130,27 @@ public class GistTests
     @Test
     public void deleteGist()
     {
-        Assert.assertTrue(true);
+        //Given I sent a request to create a Gist
+        Response response = RestAssured.given()
+                .header("Authorization", "Bearer " + TOKEN)
+                .header("Accept", "application/vnd.github+json")
+                .header("X-GitHub-Api-Version", API_VERSION)
+                .contentType(ContentType.JSON)
+                .body("{\"description\":\"" + DESCRIPTION + "\",\"public\":" + IS_PUBLIC + ",\"files\":{\"" + file1Name + "\":{\"content\":\"" + file1Content + "\"}}}")
+                .post(BASE_URI);
+
+        gistID = response.jsonPath().getString("id");
+
+        //When I send a request to delete this Gist
+                response = RestAssured.given()
+                .header("Authorization", "Bearer " + TOKEN)
+                .header("Accept", "application/vnd.github+json")
+                .header("X-GitHub-Api-Version", API_VERSION)
+                .contentType(ContentType.JSON)
+                .delete(BASE_URI + "/" + gistID);
+
+        //Then the Gist is deleted successfully
+        Assert.assertEquals(204, response.getStatusCode());
 
         //Skip cleanUp method as Gist already deleted
         skipCleanup = true;
